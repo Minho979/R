e203999deff98b2aa0f6dc2ee1579c0c471ba58a
Kim, minho <kim_979@naver.com>
2023-11-28 19:42:29 +0900

    for문을 map으로 변경하여 구현

--- study1/src/Gugudan.tsx
+++ study1/src/Gugudan.tsx
@@ -3,18 +3,27 @@ import './MyTable.css';
 
 export default function Gugudan() {
     let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let trlist = [];
-    for (let i = 2; i <= 9; i++) {
-        let mulnum = nums.map(num => 
-            <tr>{i} x {num} = {num * i}</tr>);
-        trlist.push(
-            <td key={i}>
-                <th>{i}단</th>
-                {mulnum}
-            </td>
-        );
+    let bases = [2, 3, 4, 5, 6, 7, 8, 9]
+    // let trlist = [];
+    // for (let i = 2; i <= 9; i++) {
+    //     let mulnum = nums.map(num => 
+    //         <tr>{i} x {num} = {num * i}</tr>);
+    //     trlist.push(
+    //         <td key={i}>
+    //             <th>{i}단</th>
+    //             {mulnum}
+    //         </td>
+    //     );
 
-    };
+    let trlist = bases.map(base => (
+        <td key = {base}>
+            <th>{base}단</th>
+            {nums.map(num => ( 
+                <tr key = {num}>{base} x {num} = {base * num}</tr>
+            ))}
+        </td>
+    ));
+
 
 
     return (
@@ -24,4 +33,4 @@ export default function Gugudan() {
             </tbody>
         </table>
     );
-}
+};
